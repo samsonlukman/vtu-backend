a25b63560a0b489ba7d8729caf7dd921356900f0
samsonlukman <lucassamson24@gmail.com>
2024-08-02 23:20:56 +0100

    about to try aws

--- backend/models.py
+++ backend/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    profile_pics = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    
     phone_number = models.CharField(max_length=20, null=True, blank=True)
     about = models.TextField(null=True, blank=True)
     is_verified = models.BooleanField(default=False)
@@ -11,7 +11,7 @@ class User(AbstractUser):
     account_reference = models.CharField(max_length=500, blank=True, null=True)
     account_number = models.IntegerField(blank=True, null=True)
     account_balance = models.FloatField(blank=True, null=True, default=0)
-    flutter_secret_key = models.CharField(max_length=200, default="Bearer FLWSECK-fab12578d0fa352253f89fd6a7b7b713-18f55ce05d4vt-X")
+    
 
     def increment_account_balance(self, amount):
         self.account_balance = float(amount)
@@ -21,7 +21,7 @@ class User(AbstractUser):
 
 
     def __str__(self):
-        return f"{self.username}, Phone number: {self.phone_number}, Verified: {self.is_verified}, account_balance: {self.account_balance} account_reference: {self.account_reference}, account_number: {self.account_number}, secret_key: {self.flutter_secret_key}"
+        return f"{self.username}, Phone number: {self.phone_number}, Verified: {self.is_verified}, account_balance: {self.account_balance} account_reference: {self.account_reference}, account_number: {self.account_number}"
 
 
 class Airtime(models.Model):
