f222e92f384bcde2e0aa9d18848ffdb7cc53e1b1
samsonlukman <lucassamson24@gmail.com>
2024-08-03 00:19:33 +0100

    edited ip address

--- vtu/settings.py
+++ vtu/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!k&*wkq=3qv#_lsw#ez(^bau(6!!pqj&@g^37u2=pk05hi$93b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.60.171.200:8000', 'localhost', '127.0.0.1', 'payvillesub.com', 'www.payvillesub.com']
+ALLOWED_HOSTS = ['13.60.171.200', 'localhost', '127.0.0.1', 'payvillesub.com', 'www.payvillesub.com']
 
 
 # Application definition
